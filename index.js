bfeae433f989ee68cbaf4363875209529b3c5cd1
Ezekiel Kigbo <theatlasroom@gmail.com>
2017-06-05 17:20:34 +1000

    better error handling added scripts for linting and testing updated deps

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const path = require('path')
-const CLI = require('./lib/CLI');
-const pkg = require('./package.json');
+const CLI = require('./lib/CLI')
+const pkg = require('./package.json')
 const chalk = require('chalk')
 
-const ArgumentParser = require('argparse').ArgumentParser;
+const ArgumentParser = require('argparse').ArgumentParser
 const parser = new ArgumentParser({
   version: pkg.version,
   addHelp: true,
   description: pkg.description
-});
+})
 
 parser.addArgument(
   ['-n', '--name'],
@@ -34,43 +34,51 @@ parser.addArgument(
   }
 )
 
-const args = parser.parseArgs();
+const args = parser.parseArgs()
 // console.dir(args);
-prepareOptions(args)
+try {
+  prepareOptions(args)
+} catch (e) {
+  error(e.toString())
+  bail()
+}
 
-function prepareOptions(args){
-  information('Generating your component')
-  let { name, ext, dir } = args;
+function prepareOptions (args) {
+  let { name, ext, dir } = args
 
   if (!name) throw Error(`a name must be provided`)
-  dir = dir ? dir : __dirname
+  information('Generating your component')
+  dir = dir || __dirname
   ext = (ext && ext.charAt(0) === '.') ? ext.slice(1) : 'jsx'
 
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir)
   const targetFile = path.join(dir, `${name}.${ext}`)
 
   const opts = {
-    name,
+    name
   }
 
   // TODO: do i want to first construct this obj, then call run?
   // or be able to just invoke run...
-  const cli = new CLI(opts);
+  const cli = new CLI(opts)
   cli.build((err, data) => {
-    if (err) error('ERROR:', err);
+    if (err) error('ERROR:', err)
     serializer(targetFile, data.join('\n'), (err) => {
-      if (err) throw Error(err);
-    });
-  });
-}
-
-function serializer(file, data, cb){
-  fs.writeFile(file, data, (err) => {
-    if (err) throw Error(`There was a problem creating ${file}`)
-    success(`Created file ${file}`);
+      if (err) throw Error(err)
+    })
   })
 }
 
-function information(msg){ console.error(chalk.yellow(msg)) }
-function error(msg){ console.error(chalk.red(msg)) }
-function success(msg){ console.error(chalk.green(msg)) }
+function serializer (file, data, cb) {
+  fs.writeFile(file, data, (err) => {
+    if (err) throw Error(`There was a problem creating ${file}`)
+    success(`Created file ${file}`)
+  })
+}
+
+function information (msg) { console.info(chalk.yellow(msg)) }
+function error (msg) { console.error(chalk.red(msg)) }
+function success (msg) { console.log(chalk.green(msg)) }
+function bail () {
+  process.exit(0)
+}
